0d19eb2bb60bfbdb4c95eddd11890642b1e02516
Rahat Alfaz Abir <rahatabir016@gmail.com>
2025-05-30 17:38:03 +0600

    comit 2, 5:37; 5/30/2025

--- MyBlog/Blog/views.py
+++ MyBlog/Blog/views.py
@@ -3,4 +3,7 @@ from .models import Post, Category, Tag
 # Create your views here.
 def post_list(request):
     posts = Post.objects.all()
-    return render(request, 'blog/base.html', {'posts': posts}) 
+    categories = Category.objects.all()
+    tags = Tag.objects.all()
+
+    return render(request, 'blog/base.html', {'posts': posts, 'categories':categories, 'tags':tags }) 
