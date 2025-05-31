a6306f6d949222cf2a61c78664a9911bd1229e69
Rahat Alfaz Abir <rahatabir016@gmail.com>
2025-05-31 17:48:45 +0600

    commit 3: 5/31/2025

--- MyBlog/Blog/urls.py
+++ MyBlog/Blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import post_list
+from .views import post_list, post_details
 
 urlpatterns = [
-    path("",post_list, name = 'post_list')
+    path("",post_list, name = 'post_list'),
+    path("post/<int:id>",post_details, name = 'post_details'),
 ]
--- MyBlog/Blog/views.py
+++ MyBlog/Blog/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post, Category, Tag
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+
 # Create your views here.
 def post_list(request):
     posts = Post.objects.all()
     categories = Category.objects.all()
     tags = Tag.objects.all()
 
-    return render(request, 'blog/base.html', {'posts': posts, 'categories':categories, 'tags':tags }) 
+    return render(request, 'blog/post_list.html', {'posts': posts, 'categories':categories, 'tags':tags }) 
+
+def post_details(request, id):
+    post = get_object_or_404(Post, id = id)
+    categories = Category.objects.all()
+    tags = Tag.objects.all()
+    return render(request, 'blog/post_details.html', {'post': post, 'categories':categories, 'tags':tags})
+
+def signup_view(request):
+    if(request.method == 'POST'):
+        pass
+    else:
+        form = UserCreationForm()
+    
+    return render(render, 'registration/signup.html', {'form': form})
